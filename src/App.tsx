8d06606219c29673b461aec02f4f4c41bbe93ea0
Stepan Dordiai <stepandordiai@gmail.com>
2026-06-18 11:58:20 +0200

    feat: add reset password feature and update login page

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Leads from "./pages/Leads/Leads";
 import type { Lead } from "./interfaces/Lead";
 import Home from "./pages/Home/Home";
-import "./scss/App.scss";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Clients from "./pages/Clients/Clients";
 import type { Client } from "./interfaces/Client";
+import ResetPassword from "./pages/ResetPassword/ResetPassword";
+import "./scss/App.scss";
 
 function App() {
 	const [leads, setLeads] = useState<Lead[]>([]);
@@ -33,69 +34,71 @@ function App() {
 		return () => listener.subscription.unsubscribe();
 	}, []);
 
-	const getAllLeads = async () =>
-		supabase
+	const loadLeads = async () => {
+		const { data } = await supabase
 			.from("leads")
 			.select("*")
 			.order("updated_at", { ascending: false });
-
-	const getAllClients = async () =>
-		supabase
-			.from("clients")
-			.select("*")
-			.order("updated_at", { ascending: false });
-
-	const loadLeads = async () => {
-		const { data } = await getAllLeads();
 		setLeads(data ?? []);
 	};
 
 	const loadClients = async () => {
-		const { data } = await getAllClients();
+		const { data } = await supabase
+			.from("clients")
+			.select("*")
+			.order("updated_at", { ascending: false });
 		setClients(data ?? []);
 	};
 
 	useEffect(() => {
-		if (session) loadLeads();
-	}, [session]);
-
-	useEffect(() => {
-		if (session) loadClients();
+		if (session) {
+			loadLeads();
+			loadClients();
+		}
 	}, [session]);
 
 	// TODO: learn this
-	if (authLoading) return null; // or a spinner
+	if (authLoading) return null;
 	if (!session) return <Login />;
+
 	return (
 		<Router>
-			<div className="layout">
-				<Sidebar />
-				<Routes>
-					<Route path="/" element={<Home leads={leads} />} />
-					<Route
-						path="/leads"
-						element={
-							<Leads
-								leads={leads}
-								setLeads={setLeads}
-								load={loadLeads}
-								clients={clients}
-							/>
-						}
-					/>
-					<Route
-						path="/clients"
-						element={
-							<Clients
-								clients={clients}
-								setClients={setClients}
-								load={loadClients}
-								leads={leads}
-							/>
-						}
-					/>
-				</Routes>
-			</div>
+			<Routes>
+				<Route path="/reset-password" element={<ResetPassword />} />
+				<Route
+					path="/*"
+					element={
+						<div className="layout">
+							<Sidebar />
+							<Routes>
+								<Route path="/" element={<Home leads={leads} />} />
+								<Route
+									path="/leads"
+									element={
+										<Leads
+											leads={leads}
+											setLeads={setLeads}
+											load={loadLeads}
+											clients={clients}
+										/>
+									}
+								/>
+								<Route
+									path="/clients"
+									element={
+										<Clients
+											clients={clients}
+											setClients={setClients}
+											load={loadClients}
+											leads={leads}
+										/>
+									}
+								/>
+							</Routes>
+						</div>
+					}
+				/>
+			</Routes>
 		</Router>
 	);
 }
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -7,45 +7,96 @@ const Login = () => {
 	const [password, setPassword] = useState("");
 	const [authError, setAuthError] = useState("");
 	const [authLoading, setAuthLoading] = useState(false);
+	const [forgotPassword, setForgotPassword] = useState(false);
 
 	// TODO: LEARN THIS
-	const handleAuth = async (e: React.FormEvent<HTMLFormElement>) => {
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		setAuthLoading(true);
 		setAuthError("");
 
-		const { error } = await supabase.auth.signInWithPassword({
-			email,
-			password,
-		});
-		if (error) setAuthError(error.message);
-		setAuthLoading(false);
+		try {
+			if (forgotPassword) {
+				if (!email) {
+					setAuthError("Введіть правильний електронний адрес");
+					return;
+				}
+
+				const { error } = await supabase.auth.resetPasswordForEmail(email, {
+					redirectTo: `${window.location.origin}/reset-password`,
+				});
+
+				if (error) throw error;
+				// FIXME:
+				alert("Лист для відновлення пароля відправлено");
+				setForgotPassword(false);
+				return;
+			}
+
+			const { error } = await supabase.auth.signInWithPassword({
+				email,
+				password,
+			});
+
+			if (error) throw error;
+		} catch (error) {
+			setAuthError(error instanceof Error ? error.message : "Помилка входу");
+		} finally {
+			setAuthLoading(false);
+		}
 	};
 
 	return (
-		<main className="login-main">
-			<h1 style={{ fontSize: "2rem" }}>flovas (admin)</h1>
-			<p>Please enter credentials to get access</p>
+		<main className="login">
+			<h1 style={{ fontSize: "2rem" }}>
+				<span style={{ color: "#FFA600" }}>f</span>ugo
+			</h1>
 			{authError && <strong style={{ color: "red" }}>Access denied</strong>}
-			<form className="login-form" onSubmit={handleAuth}>
+			<form className="login-form" onSubmit={handleSubmit}>
+				<p style={{ fontSize: "2rem" }}>
+					{forgotPassword ? "Забули пароль" : "Вхід"}
+				</p>
 				<div className="login-input-container">
-					<label htmlFor="">Логін</label>
+					<label htmlFor="email">Логін</label>
 					<input
+						id="email"
 						onChange={(e) => setEmail(e.target.value)}
 						value={email}
 						type="email"
+						placeholder="example@gmail.com"
 					/>
 				</div>
-				<div className="login-input-container">
-					<label htmlFor="">Пароль</label>
-					<input
-						onChange={(e) => setPassword(e.target.value)}
-						value={password}
-						type="password"
-					/>
-				</div>
-				<button className="login-submit-btn" type="submit">
-					{authLoading ? "Зачекайте..." : "Увійти"}
+				{!forgotPassword && (
+					<div className="login-input-container">
+						<label htmlFor="password">Пароль</label>
+						<input
+							id="password"
+							onChange={(e) => setPassword(e.target.value)}
+							value={password}
+							type="password"
+						/>
+					</div>
+				)}
+
+				<button
+					className="login-submit-btn"
+					type="submit"
+					disabled={authLoading}
+				>
+					{forgotPassword
+						? authLoading
+							? "Відновлення..."
+							: "Відновити пароль"
+						: authLoading
+							? "Зачекайте..."
+							: "Увійти"}
+				</button>
+				<button
+					onClick={() => setForgotPassword((prev) => !prev)}
+					style={{ textDecoration: "underline", textDecorationSkipInk: "none" }}
+					type="button"
+				>
+					{forgotPassword ? "Вхід" : "Забули пароль?"}
 				</button>
 			</form>
 		</main>
